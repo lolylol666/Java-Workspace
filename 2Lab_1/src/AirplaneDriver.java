1f2b01a67544af1f7200fdd44161a0aa951821d7
l_shiqi <l_shiqi@FETTUCCINE.ENCS.concordia.ca>
2016-01-21 17:01:06 -0500

    comp-249 lab1

---
+++ 2Lab_1/src/AirplaneDriver.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+//-----------------------------
+//COMP 249/Winter 2016
+//Date: Thursday January 21, 2016
+//Lab: 1
+//Lab Section: 
+//Your name:
+//Student ID:
+//-----------------------------
+public class AirplaneDriver {
+	public static void main(String[] args) {
+
+		final int MAX_FLEET_SIZE = 100;
+
+		Scanner keyIn = new Scanner(System.in);
+
+		
+		// b) Declare an array of objects Airplane which can hold up to MAX_FLEET_SIZE Airplane objects
+		
+
+
+		
+		// c) Create a 1st entry in the array using the default constructor
+		// which you store in the array created in step b)
+		
+
+
+
+		
+		// d) Prompt the user for the information to create 2 other objects of type Airplane and store them in the
+		// same array
+		
+
+
+		
+		// e) Increase the crew size of the 2nd Airplane by 2   
+		
+		
+
+
+		// f) Create a 4th object which has the crew size of the 3rd Airplane in Fleet and the 
+		// 1st Airplane's destination
+		
+		
+
+
+		// g) Create a new Airplane with exactly the same information as the Airplane in index location 2
+		
+
+
+
+		
+		// h) Display a message saying how many airplanes there are as well as
+		// the information of all airplanes. Make use of the static attribute numOfPlanes
+		// for the number of Airplanes.
+		
+		
+
+
+		
+		// i) Compare the Airplane in location 2 to the remaining Airplane objects in the array and 	
+		// display a message of the format
+		// airplane 2 (is/is not) the same as airplane i
+		// for the remaining airplanes, using the equals method
+
+
+
+
+
+		
+		
+		keyIn.close();
+	}// end of main
+
+}
