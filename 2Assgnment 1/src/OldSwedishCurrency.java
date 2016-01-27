43629c9166feff71f51789d92907a2444b921cf5
Shiki <Shiki@Shiki-Alienware>
2016-01-27 08:12:15 -0500

    .

--- 2Assgnment 1/src/OldSwedishCurrency.java
+++ 2Assgnment 1/src/OldSwedishCurrency.java
@@ -1,4 +1,33 @@
 
-public class OldSwedishCurrency {
-
+public class OldSwedishCurrency 
+{
+	private int riksdaler, skillings, runstyckens;
+	
+	public OldSwedishCurrency()
+	{
+		riksdaler=0;
+		skillings=0;
+		runstyckens=0;
+	}
+	
+	public OldSwedishCurrency(int ri, int sk, int ru)
+	{
+		riksdaler=ri;
+		skillings=sk;
+		runstyckens=ru;
+		normalize();
+	}
+	
+	private void normalize()
+	{
+		int ri,sk;
+		
+		sk=(runstyckens/16);
+		runstyckens-=sk*16;
+		skillings+=sk;
+		
+		ri=(skillings/48);
+		skillings-=ri*48;
+		riksdaler+=ri;
+	}
 }
