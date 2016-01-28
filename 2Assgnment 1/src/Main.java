d222ece79a844627b5af6ec520255b75be574e93
Shiki <Shiki@Shiki-Alienware>
2016-01-28 00:51:01 -0500

    Finish. only javadocs left

--- 2Assgnment 1/src/Main.java
+++ 2Assgnment 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main
@@ -12,7 +14,7 @@ public class Main
 	{
 		keyIn = new Scanner(System.in);
 		goodsArray = new HouseholdGoods[100];
-		String input;
+		int input;
 
 		System.out.println("-------------------------------------------------------------------------------");
 		System.out.println("Welcome to Shiqi Lu's marvelous Household Goods sold in swedish currency store!");
@@ -29,21 +31,35 @@ public class Main
 			System.out.println("6. Quit");
 			System.out.println("Please enter your choice --> ");
 
-			input = keyIn.nextLine();
-			input = input.trim();
-			switch (input)
+			if (keyIn.hasNextInt())
 			{
-				case "1":
-				case "2":
-				case "3":
-				case "4":
-				case "5":
-					break;
-				case "6":
-					keyIn.close();
-					System.exit(0);
-					break;
-			}
+				input = keyIn.nextInt();
+				keyIn.nextLine();
+				switch (input)
+				{
+					case 1:
+						addItem();
+						break;
+					case 2:
+						changeItem();
+						break;
+					case 3:
+						displayAllType();
+						break;
+					case 4:
+						displayUnderPrice();
+						break;
+					case 5:
+						stats();
+						break;
+					case 6:
+						System.out.println("Goodbye!");
+						keyIn.close();
+						System.exit(0);
+						break;
+				}
+			} else
+				keyIn.nextLine();
 		}
 
 	}
@@ -74,7 +90,8 @@ public class Main
 			goodsArray[index] = new HouseholdGoods(type, description, price);
 
 			index++;
-		}
+		} else
+			System.out.println("Array is full. Cannot add more than 100 items!");
 	}
 
 	private static void changeItem()
@@ -98,31 +115,231 @@ public class Main
 				return;
 		}
 
+		displayItem(itemNumber);
+
+		while (true)
+		{
+			int input;
+
+			while (true)
+			{
+				System.out.println(
+						"What would you like to change?\n1. Type\n2. Description\n3. Price\n4. Quit\nEnter choice -->");
+				if (keyIn.hasNextInt())
+				{
+					input = keyIn.nextInt();
+					if (input >= 1 && input <= 4)
+						break;
+				} else
+					keyIn.nextLine();
+			}
+
+			switch (input)
+			{
+				case 1:
+					System.out.println("Enter a new type: ");
+					goodsArray[itemNumber].setType(keyIn.nextLine());
+					displayItem(itemNumber);
+					break;
+				case 2:
+					System.out.println("Enter a new description: ");
+					goodsArray[itemNumber].setDescription(keyIn.nextLine());
+					displayItem(itemNumber);
+					break;
+				case 3:
+
+					while (true)
+					{
+						System.out.println("Enter a new price in format riksdaler, skillings and runstyckens: ");
+						int ri, sk, ru;
+						if (keyIn.hasNextInt())
+						{
+							ri = keyIn.nextInt();
+							if (keyIn.hasNextInt())
+							{
+								sk = keyIn.nextInt();
+								if (keyIn.hasNextInt())
+								{
+									ru = keyIn.nextInt();
+									goodsArray[itemNumber].setPrice(new OldSwedishCurrency(ri, sk, ru));
+									break;
+								}
+							}
+						}
+						keyIn.nextLine();
+					}
+					displayItem(itemNumber);
+					break;
+				case 4:
+					return;
+			}
+		}
+
+	}
+
+	private static void displayItem(int itemNumber)
+	{
+		System.out.println("Item #: " + itemNumber);
+		System.out.println("Type: " + goodsArray[itemNumber].getType());
+		System.out.println("Description: " + goodsArray[itemNumber].getDescription());
+		System.out.println("Price: " + goodsArray[itemNumber].getPrice().toString());
 	}
 
 	private static void displayAllType()
 	{
-
+		System.out.println("Enter the type you are searching for: ");
+		String input = keyIn.nextLine();
+		input = input.trim();
+		int count = 0;
+		for (int i = 0; i < index; i++)
+		{
+			if (goodsArray[i].getType().equals(input))
+			{
+				displayItem(i);
+				count++;
+			}
+		}
+		if (count == 0)
+			System.out.println("Nothing found.");
 	}
 
 	private static void displayUnderPrice()
 	{
+		int count = 0;
+		OldSwedishCurrency price;
 
+		while (true)
+		{
+			System.out.println(
+					"Enter the price under which you are searching for, in riksdaler, skillings and runstyckens: ");
+
+			int ri, sk, ru;
+			if (keyIn.hasNextInt())
+			{
+				ri = keyIn.nextInt();
+				if (keyIn.hasNextInt())
+				{
+					sk = keyIn.nextInt();
+					if (keyIn.hasNextInt())
+					{
+						ru = keyIn.nextInt();
+						price = new OldSwedishCurrency(ri, sk, ru);
+						break;
+					}
+				}
+			}
+			keyIn.nextLine();
+		}
+
+		for (int i = 0; i < index; i++)
+		{
+			if (price.compareTo(goodsArray[i].getPrice()) > 0)
+			{
+				displayItem(i);
+				count++;
+			}
+		}
+
+		if (count == 0)
+			System.out.println("Nothing found.");
 	}
 
 	private static void stats()
 	{
-
+		while (true)
+		{
+			System.out.println(
+					"What information would you like?\n1. Cost and details of cheapest item\n2. Cost and details of most costly item\n3. Number of items of a each type\n4. Average cost of items in inventory\n5. Quit\nEnter your choice >");
+			if (keyIn.hasNextInt())
+			{
+				int input = keyIn.nextInt();
+				keyIn.nextLine();
+				if (input > 0 && input < 6)
+				{
+					switch (input)
+					{
+						case 1:
+							for (int i = 0; i < index; i++)
+							{
+								if (lowestPrice() == goodsArray[i].getPrice().convertToRunstycken())
+									displayItem(i);
+							}
+							break;
+						case 2:
+							for (int i = 0; i < index; i++)
+							{
+								if (highestPrice() == goodsArray[i].getPrice().convertToRunstycken())
+									displayItem(i);
+							}
+							break;
+						case 3:
+							List<String> goodsList = new ArrayList<String>();
+							for (int i = 0; i < index; i++)
+							{
+								if (!goodsList.contains(goodsArray[i].getType()))
+								{
+									goodsList.add(goodsArray[i].getType());
+								}
+							}
+							for (String itemType : goodsList)
+							{
+								int typeNumber = 0;
+								for (int i = 0; i < index; i++)
+								{
+									if (goodsArray[i].getType().equals(itemType))
+										typeNumber++;
+								}
+								System.out.println("Type: " + itemType);
+								System.out.println("Number: " + typeNumber + "\n");
+							}
+							break;
+						case 4:
+							int sum = 0;
+							for (int i = 0; i < index; i++)
+							{
+								sum += goodsArray[i].getPrice().convertToRunstycken();
+							}
+							double average = (double) (sum / (index - 1));
+							double decimal = (average % 1);
+							OldSwedishCurrency avPrice = new OldSwedishCurrency(0, 0, (int) average);
+							System.out.println(
+									"The average price is: " + avPrice.toString() + " and" + decimal + "runstyckens.");
+							break;
+						case 5:
+							return;
+					}
+				}
+			} else
+				keyIn.nextLine();
+		}
 	}
 
-	private static HouseholdGoods lowestPrice()
+	private static int lowestPrice()
 	{
-		return new HouseholdGoods();
+		int result = 0;
+		for (int i = 0, comp = goodsArray[0].getPrice().convertToRunstycken(); i < index; i++)
+		{
+			if (comp > goodsArray[i].getPrice().convertToRunstycken())
+			{
+				comp = goodsArray[i].getPrice().convertToRunstycken();
+			}
+			result = comp;
+		}
+		return result;
 	}
 
-	private static HouseholdGoods highestPrice()
+	private static int highestPrice()
 	{
-		return new HouseholdGoods();
+		int result = 0;
+		for (int i = 0, comp = goodsArray[0].getPrice().convertToRunstycken(); i < index; i++)
+		{
+			if (comp < goodsArray[i].getPrice().convertToRunstycken())
+			{
+				comp = goodsArray[i].getPrice().convertToRunstycken();
+			}
+			result = comp;
+		}
+		return result;
 	}
 
 	private static boolean checkPass()
--- Test/src/TestMain.java
+++ Test/src/TestMain.java
@@ -7,14 +7,17 @@ public class TestMain
 {
 	private int	i		= 0;
 	BigDecimal	value	= new BigDecimal(1.0);
-						
+
 	public static void main(String[] args)
 	{
+		double d = 4.23;
+		double e = d % 1;
+		System.out.println(e);
 		String spaces = new String(new char[10]).replace("\0", "ok");
 		System.out.println(spaces);
 		int x = 50, y = 20, i = 6;
 		float z = 5 / 10;
-		
+
 		System.out.println(z * (x * 100 / y));
 		TestInterface max = (a, b) ->
 		{
@@ -35,17 +38,17 @@ public class TestMain
 		Timer timer = new Timer(1000, null);
 		timer.setCoalesce(true);
 	}
-	
+
 	/*
 	 * public int max(int a, int b) { // TODO Auto-generated method stub return
 	 * (a>b)?a:b; }
 	 */
-	
+
 	static int caca(int a, int b, TestInterface face)
 	{
 		return face.op(a, b);
 	}
-	
+
 	public void run()
 	{
 		TestClass1<Integer> caca = new TestClass1<Integer>();
