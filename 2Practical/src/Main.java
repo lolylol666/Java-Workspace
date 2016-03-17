c3f3d4af8ed5b9a1c3b344a40df2c7ad6293a778
l_shiqi <l_shiqi@CANNELLONI.ENCS.concordia.ca>
2016-03-17 17:04:20 -0400

    Practical for 249

---
+++ 2Practical/src/Main.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		String directory;
+
+		System.out.println("Enter a directory or file:");
+		directory = input.nextLine();
+
+		System.out.println(getSize(new File(directory)) + " bytes");
+	}
+
+	public static long getSize(File file)
+	{
+		long size = 0;
+		File[] files;
+
+		if (file.exists())
+		{
+			if (file.isDirectory())
+			{
+				files = file.listFiles();
+				for (int i = 0; i < files.length; i++)
+				{
+					size += getSize(files[i]);
+				}
+			} else
+			{
+				return file.length();
+			}
+		} else
+			System.out.println("File does not exist.");
+		return size;
+	}
+
+}
