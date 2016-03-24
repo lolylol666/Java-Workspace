8f718ceb8bfe0975a8296e0089de0c7c8431a73b
l_shiqi <l_shiqi@VERMICELLI.ENCS.concordia.ca>
2016-03-24 16:50:48 -0400

    2Lab7 finish

--- 2Lab7/src/Gradebook.java
+++ 2Lab7/src/Gradebook.java
@@ -1,5 +1,40 @@
 
-public class Gradebook
+public class Gradebook<E>
 {
+	private E[] grades;
 
+	public Gradebook(E[] array)
+	{
+		this.grades = array;
+	}
+
+	public E[] getGrades()
+	{
+		return grades;
+	}
+
+	public void setGrades(E[] array)
+	{
+		this.grades = array;
+	}
+
+	public void printGrades()
+	{
+		for (int i = 0; i < grades.length; i++)
+		{
+			System.out.print(grades[i] + " ");
+		}
+		System.out.print("\n");
+	}
+
+	public int numGrades(E grade)
+	{
+		int count = 0;
+		for (int i = 0; i < grades.length; i++)
+		{
+			if (grades[i].equals(grade))
+				count++;
+		}
+		return count;
+	}
 }
